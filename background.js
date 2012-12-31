616c39c57ad2ed67b237fbc653b79ff26a4a2ea5
Jeremy Heiner <ScalaProtractor@gmail.com>
2012-12-30 20:19:10 -0500

    preparing to add options
    will want a place to change color/size of pencil, etc.
    from the 0728 archive folder

--- background.js
+++ background.js
@@ -15,6 +15,10 @@ function disconnect(port) { port.disconnect(); }
 var tabHash = {}; var portCount = 0;
 notifyState(false); notifyCount(0);
 
+function notifyTabState(tabId) {
+	var found = tabHash[tabId];
+	notifyState( (!found) ? false : found.isActive ); }
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var found = tabHash[tab.id];
 	if (found) found.toggle(); else {
@@ -35,10 +39,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 				notifyState(false); } };
 		chrome.tabs.executeScript(tab.id,injected); } });
 
-chrome.tabs.onActivated.addListener(function(info) {
-	var found = tabHash[info.tabId];
-	notifyState( (!found) ? false : found.isActive ); });
-
 chrome.extension.onConnect.addListener(function(port) {
 	var found = tabHash[port.sender.tab.id];
 	if (!found) {
@@ -47,5 +47,15 @@ chrome.extension.onConnect.addListener(function(port) {
 	else {
 		if (found.isActive) { sendAttach(port); notifyState(true); }
 		found.ports.push(port); notifyCount(++portCount);
-		port.onDisconnect.addListener(function() {found.destroy()}); } });
+		port.onDisconnect.addListener(function() {found.destroy()});
+		port.onMessage.addListener(function(message) {
+			if (message=="toggle") found.toggle(); }); } });
+
+chrome.tabs.onActivated.addListener(function(info) {
+	notifyTabState(info.tabId); });
+
+chrome.windows.onFocusChanged.addListener(function(winId) { if (winId != -1)
+	chrome.tabs.query( { active:true, windowId:winId }, function(tabs) {
+		if (tabs.length != 1) console.error(tabs);
+		else notifyTabState(tabs[0].id); }); });
 
--- inject.js
+++ inject.js
@@ -23,110 +23,103 @@ svg.setAttribute("stroke","black");
 svg.setAttribute("fill","none");
 document.body.appendChild(svg);
 
-// keyboard events don't have mouse position, so...
+function eraseAt(x,y,subsequent) {
+	var found = false;
+	for ( var hit = document.elementFromPoint(x,y)
+	      ; hit && hit.parentNode == svg
+	      ; hit = document.elementFromPoint(x,y) ) {
+		if (subsequent) while (svg.lastChild != hit)
+			svg.removeChild(svg.lastChild);
+		svg.removeChild(hit); found = true; }
+	return found; }
 
-var clientX; var clientY;
-function trackMouse(event) {
-	clientX = event.clientX; clientY = event.clientY; }
+var pressed = false; // is the primary mouse button down?
+var doodle = null; // the path currently being drawn
+var lastX = 0; var lastY = 0; // pageX,pageY of last doodle point
 
-var trackX; var trackY; var trackFromPoint;
-var trackChange = function(was,hit) {
-	// should probably limit focus to text inputs...
-	hit.focus(); return hit; }
-function trackElement() {
-	if (trackX != clientX || trackY != clientY ) {
-		trackX = clientX; trackY = clientY;
-		svg.style.visibility="hidden";
-		var element = document.elementFromPoint(trackX,trackY);
-		svg.style.visibility="visible";
-		if ( element != trackFromPoint )
-			trackFromPoint = trackChange( trackFromPoint, element ); } }
-
-var attachKeyboard = function() {
-	document.addEventListener("keydown",trackElement);
-	document.addEventListener("keypress",trackElement);
-	document.addEventListener("keyup",trackElement); };
-var detachKeyboard = function() {
-	document.removeEventListener("keydown",trackElement);
-	document.removeEventListener("keypress",trackElement);
-	document.removeEventListener("keyup",trackElement); };
-
-// new doodle being doodled, and previous doodle path moused down on
-// (which could turn out to be a click on that path or start of a new doodle)
-
-var doodle; var pressPath; var pressX; var pressY;
-function thunkPress(path) { return function(event) {
-	pressPath = path; downMouse(event); }}
+// keyboard events don't have mouse position (needed for elementFromPoint)
+var clientX = 0; var clientY = 0; // so stash them here in all mouse events
 
 function downMouse(event) {
-	pressX = event.pageX; pressY = event.pageY;
-	trackMouse(event); event.preventDefault();
-	svg.addEventListener("mousemove",moveMouse);
-	svg.addEventListener("mouseup",upMouse); }
+	clientX = event.clientX; clientY = event.clientY;
+	if (0 != event.button) return;
+	if (pressed || doodle) { console.error("lost mouseup?"); doodle = null; }
+	lastX = event.pageX; lastY = event.pageY;
+	pressed = true; event.preventDefault(); }
 
 function moveMouse(event) {
-	trackMouse(event);
-	if (event.pageX != pressX || event.pageY != pressY) {
-		if (!doodle) {
-			doodle = document.createElementNS(svgNS,"path");
-			doodle.setAttribute("d","M "+pressX+" "+pressY);
-			doodle.addEventListener("mousedown",thunkPress(doodle));
-			svg.appendChild(doodle); pressPath = null; }
-		var data = doodle.getAttribute("d");
-		var add = " "+event.pageX+" "+event.pageY;
-		if (data.substr(data.length-add.length) != add) {
-			data = data+" L"+add; doodle.setAttribute("d",data); } } }
+	clientX = event.clientX; clientY = event.clientY;
+	if (!pressed) return;
+	event.preventDefault();
+	var deltaX = event.pageX - lastX; var deltaY = event.pageY - lastY;
+	if (deltaX*deltaX + deltaY*deltaY < 17) return;
+	if (doodle)
+		doodle.setAttribute("d",doodle.getAttribute("d")
+		                    + " L "+event.pageX+" "+event.pageY);
+	else {
+		doodle = document.createElementNS(svgNS,"path");
+		doodle.setAttribute("d","M "+lastX+" "+lastY
+		                    + " L "+event.pageX+" "+event.pageY);
+		svg.appendChild(doodle); }
+	lastX = event.pageX; lastY = event.pageY; }
 
 function upMouse(event) {
 	moveMouse(event);
-	if (pressPath) {
-		svg.removeChild(pressPath);
-		pressPath = null; }
-	else { if (!doodle) {
-		doodle = document.createElementNS(svgNS,"circle");
-		doodle.setAttribute("r",3);
-		doodle.setAttribute("cx",pressX);
-		doodle.setAttribute("cy",pressY);
-		doodle.setAttribute("fill","black");
-		doodle.setAttribute("stroke","none");
-		doodle.addEventListener("mousedown",thunkPress(doodle));
-		svg.appendChild(doodle);
-	} doodle = null; }
-	svg.removeEventListener("mousemove",moveMouse);
-	svg.removeEventListener("mouseup",upMouse); }
+	if (0 != event.button) return;
+	event.preventDefault();
+	if (!pressed) console.error("lost mousedown?"); pressed = false;
+	if (doodle) { doodle = null; return; }
+	// svg.getIntersectionList is bounding-box based, hence useless
+	// don't require precision clicks to erase
+	var found = false; var subsequent = event.ctrlKey;
+	if (eraseAt(clientX,clientY,subsequent)) found = true;
+	if (eraseAt(clientX+3,clientY+3,subsequent)) found = true;
+	if (eraseAt(clientX-3,clientY-3,subsequent)) found = true;
+	if (eraseAt(clientX-3,clientY+3,subsequent)) found = true;
+	if (eraseAt(clientX+3,clientY-3,subsequent)) found = true;
+	if (found) return;
+	var dot = document.createElementNS(svgNS,"circle");
+	dot.setAttribute("r",3);
+	dot.setAttribute("cx",lastX);
+	dot.setAttribute("cy",lastY);
+ 	dot.setAttribute("fill","black");
+	dot.setAttribute("stroke","none");
+	svg.appendChild(dot); }
 
-// attach and detach all our event handlers
-
-var isActive = false;
-function attachListeners() { if (!isActive) {
-	svg.setAttribute("pointer-events","visiblePainted");
-	svg.addEventListener("mousedown",downMouse);
-	document.addEventListener("mousemove",trackMouse);
-	attachKeyboard();
-	isActive = true; }}
-function detachListeners() { if (isActive) {
+var focusX; var focusY; var focusOn;
+function focusToMouse() {
+	if (focusX == clientX && focusY == clientY ) return;
+	focusX = clientX; focusY = clientY;
 	svg.setAttribute("pointer-events","none");
-	svg.removeEventListener("mousedown",downMouse);
-	document.removeEventListener("mousemove",trackMouse);
-	detachKeyboard();
-	isActive = false; }}
+	var hit = document.elementFromPoint(focusX,focusY);
+	svg.setAttribute("pointer-events","visiblePainted");
+	if (!hit || hit == focusOn) return;
+	hit = focusTest( hit );
+	if (hit) focusOn = hit; }
+var focusTest = function(hit) {
+	switch (hit.nodeName) {
+	case "INPUT": case "BUTTON": case "SELECT": case "TEXTAREA":
+		hit.focus(); return hit; }
+	return null; }
 
+
+/*
 if (/^http:\/\/www\.puzzlemix\.com\/playgrid/.test(document.documentURI)) {
-	trackChange = function(was,hit) {
+	focusTest = function(was,hit) {
 		// if (hit.nodeName == "INPUT") { hit.click(); return hit; } ???
 		var cell = hit;
 		while (cell && ! /select_cell\(\d+\)/.test(cell.onclick))
 			cell = cell.parentNode;
 		if (cell && was != cell) {
-			/*console.log(cell.id+".click");*/ cell.click(); return cell; }
+			/x*console.log(cell.id+".click");*x/ cell.click(); return cell; }
 		return was; }
 	var original = document.body.onkeydown;
-	var tracking = function(event) { trackElement(); original(event); }
+	var tracking = function(event) { focusToMouse(); original(event); }
 	attachKeyboard = function() { document.body.onkeydown = tracking; };
 	detachKeyboard = function() { document.body.onkeydown = original; }; }
 else
 if (/^http:\/\/www\.sudoku\.org\.uk\/Puzzles/.test(document.documentURI)) {
-/*
+/x*
 for <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">s
   var param = document.createElement('param');
     param.setAttribute('name','wmode');
@@ -135,7 +128,7 @@ for <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">s
   var wrapper = document.createElement('div');
     obj.parentNode.replaceChild(wrapper,obj.parentNode);
 	wrapper.appendChild(obj)
-*/
+*x/
 	var embeds = document.getElementsByTagName("EMBED")
 	for (var i = embeds.length - 1 ; i >= 0 ; --i ) {
 		if (embeds[i].getAttribute("type")=="application/x-shockwave-flash") {
@@ -145,27 +138,37 @@ for <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000">s
 				var grandp = parent.parentElement;
 				grandp.replaceChild(embeds[i],parent);
 				parent = grandp; } } } }
+*/
 
 // communication with the extension
 
 var port = chrome.extension.connect({name:document.documentURI});
-// in a frameset tab.url isn't very helpful
-
-function keybindingHack(event) {
-	// experimental.keybinding only supports alpha keys ('A' to 'Z')
-	if (event.keyCode == 107) // numpad +
-		port.postMessage("toggle"); }
-window.addEventListener("keydown",keybindingHack);
 
 port.onDisconnect.addListener(function() {
 	detachListeners();
 	window.removeEventListener("resize",svgResize);
-	window.removeEventListener("keydown",keybindingHack);
 	document.body.removeChild(svg); });
+
 port.onMessage.addListener(function(message) {
 	switch (message) {
 		case "attach": attachListeners(); break;
 		case "detach": detachListeners(); break; } });
+
+var isActive = false;
+function attachListeners() { if (!isActive) {
+	svg.setAttribute("pointer-events","visiblePainted");
+	svg.addEventListener("mousedown",downMouse);
+	svg.addEventListener("mousemove",moveMouse);
+	svg.addEventListener("mouseup",upMouse);
+	window.addEventListener("keydown",focusToMouse,true);
+	isActive = true; }}
+function detachListeners() { if (isActive) {
+	svg.setAttribute("pointer-events","none");
+	svg.removeEventListener("mousedown",downMouse);
+	svg.removeEventListener("mousemove",moveMouse);
+	svg.removeEventListener("mouseup",upMouse);
+	window.removeEventListener("keydown",focusToMouse,true);
+	isActive = false; }}
 
 } // if not frameset
 
